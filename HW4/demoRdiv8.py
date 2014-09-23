0623e2571c390746cd82bdb9e66ca1b1d0052e3c
WeiFoo <wfu@ncsu.edu>
2014-09-23 00:37:44 -0400

    update

--- HW4/demoRdiv8.py
+++ HW4/demoRdiv8.py
@@ -5,8 +5,9 @@ sys.dont_write_bytecode = True
 
 def rdiv8():
   rdivDemo([
-      ["banana", 1.5, 1.3,  1.4,  1.7,  2.1],
-      ["grapes", 0.2, 0.18, 0.21, 0.23, 0.23],
-      ["pineapples", 12, 11.4, 13, 12.23, 11.23],
+      ["orange", 5.7, 5.1, 4.6, 4.9, 4.6],
+      ["apple", 5.3, 5.6, 4.8, 5.2, 4.7],
+      ["pears", 4.2, 4.4, 4.1, 4.5, 4,8, 5.0]
+      ["strawberry", 0.33, 0.35, 0.31, 0.36, 0.34]
       ])
 if __name__ == "__main__": eval("rdiv8()")
