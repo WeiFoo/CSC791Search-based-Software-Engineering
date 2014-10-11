9a42af9417f28bccfaf7f8d8b3fc2e633bb4a244
WeiFoo <wfu@ncsu.edu>
2014-10-11 17:07:15 -0400

    update

--- HW4redo/sa.py
+++ HW4redo/sa.py
@@ -2,8 +2,6 @@ from __future__ import division
 import sys, random, math
 from models import *
 from base import *
-from log import *
-
 sys.dont_write_bytecode = True
 @printlook      
 def sa(model):
